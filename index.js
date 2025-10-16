cdde68a65357030324943756ffbe9b6096bb9fc2
luke <lfairbanks@anamosa.k12.ia.us>
2025-10-16 18:16:32 +0000

    done

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-
-
+let hamlet = 'To be, or not to be, that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune,
+Or to take Arns against a Sea of trouble:
 let length = hanlet.length;
-
-let firstLetter = hamlet[0]⑆
-
-let secondLetter = hanlet[1];
+let firstLetter = hamlet[0l;
+let secondLetter = hamlet [1];
 let lastLetter = hamlet length 11;
-
-document.getElenentById("length").innerHTML=
-length;
-
-document.getElenentById("first-letter").innerHTML = firstLetter;
-
-document.getElementById("second-letter").innerHTML
-secondLetter;
-
+document.getElenentById("length"). innerHTML Length;
+document.getElementById("first-letter").innerHTML = firstLetter;
+document.getElenentById("second-letter").innerHTML secondLetter;
 document.getElementById("last-letter").innerHTML = lastLetter;
