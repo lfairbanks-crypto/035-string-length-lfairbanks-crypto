24d482d3fb3c0fcb4b05fc3da4ef1273d8495fb3
luke <lfairbanks@anamosa.k12.ia.us>
2025-10-16 18:10:32 +0000

    done

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
-//Add code below
-1
-2
-008-string-length > 001-lecture > s index.js> (e) lastLetter
-let hamlet = 'To be, or not to be, that is the question:
-Whether 'tis nobler in the mind to suffer
-3
-The slings and arrows of outrageous fortune,
-4
-Or to take Arms against a Sea of trouble:
-5
-6
+
 let length = hanlet.length;
-7
+
 let firstLetter = hamlet[0]⑆
-6
-B
+
 let secondLetter = hanlet[1];
 let lastLetter = hamlet length 11;
-18
-11
+
 document.getElenentById("length").innerHTML=
 length;
-12
+
 document.getElenentById("first-letter").innerHTML = firstLetter;
-13
+
 document.getElementById("second-letter").innerHTML
 secondLetter;
-14
+
 document.getElementById("last-letter").innerHTML = lastLetter;
