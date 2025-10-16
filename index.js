eaed596f44dcd11f0369951fa88907eecd1d449f
luke <lfairbanks@anamosa.k12.ia.us>
2025-10-16 18:24:35 +0000

    done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
-let hamlet = 'eeeeeee:
+let hamlet = 'To be, or not to be, that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune,
+Or to take Arns against a Sea of trouble:
 let length = hanlet.length;
 let firstLetter = hamlet[0l;
 let secondLetter = hamlet [1];
